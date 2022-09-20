794fb47d5f70747cff3090eeca70ce99a2400692
birrut <chaze_birrut@protonmail.com>
2022-09-19 21:18:47 -0700

    Add config template

---
+++ config_template.py
@@ -0,0 +1,8 @@
+#rename this file to config.py and fill in your bot's codes
+
+
+# Ally code for member of desired guild
+ALLY_CODE = 'XXXXXXXXX'
+S_AUTH = 'SUPER SECRET CODE'
+BOT_AUTH_CODE = 'SUPER SECRET AUTH CODE'
+BOT_VERSION = 'bot version, if desired'
